3ed3c6d7bbd57d97ced917499af60c04fbee153e
malaya1855 <121491813+malaya1855@users.noreply.github.com>
2023-03-25 13:29:25 +0000

    task-3

--- index.js
+++ index.js
@@ -21,7 +21,29 @@
  *?      https://developer.mozilla.org/ru/docs/Web/JavaScript/Reference/Global_Objects/String/padStart
  *?      70 === 01:10
  */
-const time = prompt('Яка година?');
-const hours = String(Math.floor(time / 60)).padStart(2, 0);
-const minutes = String(time % 60).padStart(2, 0);
-console.log(`${hours}:${minutes}`) 
+// const time = prompt('Яка година?');
+// const hours = String(Math.floor(time / 60)).padStart(2, 0);
+// const minutes = String(time % 60).padStart(2, 0);
+// console.log(`${hours}:${minutes}`) 
+/**
+ *? Напишіть цикл, який виводить у консоль
+ *? числа від max до min за спаданням
+ *? Виведіть у консоль усіх парних чисел від min до max
+ */
+
+ const a = 50;
+ const b = 23;
+ function numbers (num1, num2) {
+    for (let i = num1; i >= num2; i -= 1) {
+        console.log (i);
+        }
+        console.log ("Парні числа")
+        for (let i = num2; i <= num1; i += 1) {
+        if (i % 2 === 0) {
+       console.log (i)
+        }
+       } 
+ }
+ numbers (a, b);
+
+    
